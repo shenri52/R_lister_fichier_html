3567ddcea8880a8aa4da8c84da2c6240b180bd59
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2025-03-17 08:51:58 +0100

    Amélioration mineure

--- LISTER_FICHIER_HTML/script/arborescence.R
+++ LISTER_FICHIER_HTML/script/arborescence.R
@@ -16,16 +16,15 @@ list_dossier <- list.files(path = lecteur,
                            file.info() %>%
                            select(mtime)
 
-# Modification du format de la date
-list_dossier$mtime <- as_date(list_dossier$mtime)
-  
-# Création d'un dataframe avec le nom du chemin, le nom du fichier et le type d'extension
+# Mettre en forme la liste des fichiers
 list_dossier <- list_dossier %>%
-                mutate(Chemin = dirname(row.names(list_dossier))) %>%
-                mutate(Fichier = basename(row.names(list_dossier))) %>%
-                mutate(Extension = as.factor(toupper(sub("^.+\\.", "", Fichier)))) %>%
+                # Ajouter le chemin, le nom du fichier, l'extension et la date de modification formatée
+                mutate(Chemin = dirname(row.names(list_dossier)),
+                       Fichier = basename(row.names(list_dossier)),
+                       Extension = as.factor(toupper(sub("^.+\\.", "", Fichier))),
+                       mtime = as_date(list_dossier$mtime)) %>%
+                # Choisir l'ordre des colonnes
                 select(Chemin, Fichier, Extension, mtime)
   
-# Suppression des noms de ligne
+# Supprimmer les noms de ligne
 row.names(list_dossier) <- NULL
-            
